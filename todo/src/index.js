74076471a620f1cd2ea6acab3d0d62fa8253ad0e
charoufnassro <charoufnassro@gmail.com>
2019-07-06 14:34:56 +0200

    test import modules

--- todo/src/index.js
+++ todo/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import Primer from './Primer'
 
 import 'bootstrap/dist/css/bootstrap.css'
 
